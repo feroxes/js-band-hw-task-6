96245b067af7dc55f504fd42cb0742e3a4a6b581
Yaroslav Harmash <feroxes@gmail.com>
2019-11-30 16:03:34 +0200

    CHW-001: + routing added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Menu from './components/Menu';
+import TodoList from './containers/TodoList';
+import Footer from './components/Footer';
+import NotFound from './containers/NotFound';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return <div>feroxes</div>;
-  }
+function App() {
+  return (
+    <div className="app">
+      <Menu />
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/todos" />
+        </Route>
+        <Route exact path="/todos" component={TodoList} />
+        <Route component={NotFound} />
+      </Switch>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
