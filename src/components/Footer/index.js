689d5453631933966d9e8edbeb713bfbb61484b7
Yaroslav Harmash <feroxes@gmail.com>
2019-12-02 16:17:02 +0200

    CHW-008: + footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 function Footer() {
-  return <div />;
+  return (
+    <div className="position-absolute fixed-bottom text-center">
+      <span>
+        <code>
+          created by <a href="https://github.com/feroxes/">feroxes</a>
+        </code>
+      </span>
+    </div>
+  );
 }
 
 export default Footer;
